5f19b0e6714f05571a46d20f5caf6b89cec20085
Tacocat3 <yongjusung3351@gmail.com>
2022-03-08 03:07:54 +0900

    리스트 조회, 태그 조회 구현

--- routes/script.js
+++ routes/script.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { scriptsList, scriptDetail } = require("../controller/script");
+const { scriptsListId, scriptTag, scriptDetail } = require("../controller/script");
 const  postScript = require("../migration/insert_Scripts")
 
 router.post("/script", postScript.postScript);
-// router.get("/script/:tagId", scriptsList);
-// router.get("/detail/:scriptId", scriptDetail);
+router.get("/script/list/:scriptListId", scriptsListId);
+router.get("/script/tag/:scriptTag", scriptTag);
+ //router.get("/detail/:scriptId", scriptDetail);
 
 module.exports = router;

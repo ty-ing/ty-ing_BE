c00eea6e15785430c679e5292bbe1d138eeec742
Tacocat3 <yongjusung3351@gmail.com>
2022-03-07 21:21:15 +0900

    escaped password character modify

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect('mongodb://dbadmin:z\!\+Qz8K\?\#9\;rSwHK@localhost:27017/Real-project', {
+    mongoose.connect('mongodb://dbadmin:z!+Qz8K?#9;rSwHK@localhost:27017/Real-project', {
+        useNEWURLParser: true,
         ignoreUndefined: true
     }).catch(err => console.log(err));
 };

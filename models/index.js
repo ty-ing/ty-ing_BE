6b8c9da6cd8bcda565cadcc207e15fa201dd60a7
Tacocat3 <yongjusung3351@gmail.com>
2022-03-07 23:07:17 +0900

    1

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect('mongodb://admin:VaJhBS5xmgLLch2y@localhost:27107/server',
+    mongoose.connect('mongodb://admin:VaJhBS5xmgLLch2y@localhost:27107/admin',
+    
     //mongoose.connect('mongodb://localhost:27017/Real-project',
      {
+         dbName:"server",
     }).catch(err => console.log(err));
 };
 

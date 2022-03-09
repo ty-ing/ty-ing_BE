70e3bd5cdaaa1035578d5c308b67c82912609d84
Tacocat3 <yongjusung3351@gmail.com>
2022-03-10 02:57:04 +0900

    3

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -18,6 +18,9 @@ module.exports = (req, res, next) => {
   // 로그인정보 저장해놓기
   try {
     const { id } = jwt.verify(authToken, process.env.TOKENKEY);
+    console.log("1123", id)
+    console.log("1124", authToken)
+    console.log("1111", authType)
     Users.findById(id).then((user) => {
       res.locals.user = user; //굳이 데이터베이스에서 사용자 정보를 가져오지 않게 할 수 있도록 express가 제공하는 안전한 변수에 담아두고 언제나 꺼내서 사용할 수 있게함
       next();

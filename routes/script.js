b922131ca4552eac2f6583401f3ca5e815c8f48c
Tacocat3 <yongjusung3351@gmail.com>
2022-03-15 17:41:34 +0900

    필터링 페이지 기능, 검색 기능 구현

--- routes/script.js
+++ routes/script.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { scriptsListId, scriptTag, scriptDetail } = require("../controller/script");
-const  postScript = require("../migration/insert_Scripts")
+const {
+  findScript,
+  scriptFilter,
+  scriptDetail,
+  searchScripts,
+} = require("../controller/script");
+const postScript = require("../migration/insert_Scripts");
 
 router.post("/script", postScript.postScript);
-router.get("/script/list/:scriptListId", scriptsListId);
-router.get("/script/tag/:scriptTag", scriptTag);
+router.get("/script/:scriptType/:scriptCategory", findScript);
+router.get("/script/list", scriptFilter);
+router.get("/search", searchScripts);
 router.get("/detail/:scriptId", scriptDetail);
 
 module.exports = router;

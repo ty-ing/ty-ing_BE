4920840866c971cfc34284343da1e1203716fdc0
nklee6300 <nogi3001@gmail.com>
2022-03-15 22:23:57 +0900

    nickname type 변경

--- passport/index.js
+++ passport/index.js
@@ -30,7 +30,7 @@ module.exports = () => {
               // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
               const newNickname = Date.now();
               const newUser = await Users.create({
-                nickname: newNickname,
+                nickname: newNickname.toString(),
                 id: profile.id,
                 provider: "kakao",
               });

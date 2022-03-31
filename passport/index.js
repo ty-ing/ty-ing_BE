a0136a8fafbbe6928dda826279ec55764f7c7da0
nklee6300 <nogi3001@gmail.com>
2022-03-31 23:55:42 +0900

    카카오 로그인 테스트용 콘솔 삭제

--- passport/index.js
+++ passport/index.js
@@ -16,8 +16,6 @@ module.exports = () => {
         // accessToken, refreshToken: 로그인 성공 후 카카오가 보내준 토큰
         // profile: 카카오가 보내준 유저 정보. profile의 정보를 바탕으로 회원가입
         async (accessToken, refreshToken, profile, done) => {
-          console.log("토큰 :", accessToken);
-          console.log("kakao profile", profile);
           try {
             const exUser = await Users.findOne({
               // 카카오 플랫폼에서 로그인 했고 & snsId필드에 카카오 아이디가 일치할경우
